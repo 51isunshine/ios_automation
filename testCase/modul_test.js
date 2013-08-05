00097f26574156d72f34496cb920b3415d4d564a
51isunshine <dylan.zhangzhi@gmail.com>
2013-08-05 10:34:58 +0800

    Update modul_test.js

--- testCase/modul_test.js
+++ testCase/modul_test.js
@@ -1,5 +1,6 @@
 /**
  *根据应用的模块，专门用于测试
+ * run test-script under commond line mode with node.js.So please install node.js first.
  */
 #import "./JS/imports.js"
 var testSuit={};
@@ -11,3 +12,12 @@ testSuit.exampleTwo1=appTest("exampleTwo1",exampleTwo1)
  */
 var path="/Users/dylan/Documents/ringcentral/Spartan/LibJS/"; // Please input your "File Path" instead my "File Path"
 node(testSuit,path);
+
+
+/**
+ * if you want to run test-script in instruments,please invoking this file .
+ * Run test-script under 'instruemnts' mode
+ */
+ #import "./JS/imports.js"
+appTest("exampleTwo",exampleTwo);
+appTest("exampleTwo1",exampleTwo1)
